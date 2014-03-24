8e5478e98a1ec9ea1f11bb85523d7040809e6b1c
michael.p <michael.p@agentsofvalue.com>
2014-03-24 18:03:08 +0800

    merge files

--- js/main.js
+++ js/main.js
@@ -5,20 +5,12 @@ var app = {
 		// Check of browser supports touch events...
 		if (document.documentElement.hasOwnProperty('ontouchstart')) {
 			// ... if yes: register touch event listener to change the "selected" state of the item
-			$('body').on('touchstart', 'a', function(event) 
-				$(this).on('touchend', function(e) {
-					$(this).off('touchend');
-					$(event.target).removeClass('tappable-active');
-				});
-				$(this).on('touchmove', function(e){
-					$(this).off('touchend');
-					});
-				}
+			$('body').on('touchstart', 'a', function(event) {
 				$(event.target).addClass('tappable-active');
 			});
-			/*$('body').on('touchend', 'a', function(event) {
+			$('body').on('touchend', 'a', function(event) {
 				$(event.target).removeClass('tappable-active');
-			});*/
+			});
 		} else {
 			// ... if not: register mouse events instead
 			$('body').on('mousedown', 'a', function(event) {
